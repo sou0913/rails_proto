e4af3d2b9bf6b85be6d536af9fbe8d27864d182f
sou <sasaki.sou@samurai-adways.net>
2020-08-01 23:42:55 +0900

    corsなど

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,4 +3,10 @@ class EmployeesController < ApplicationController
     employees = Employee.all
     render json: employees
   end
+
+  # GET employees/1
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee
+  end
 end
