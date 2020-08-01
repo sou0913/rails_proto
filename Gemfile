ed00d95817e3941de757c62ed66b55f99cbe9065
sou <sasaki.sou@samurai-adways.net>
2020-08-01 22:38:15 +0900

    sqlite3に切り替え

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.5'
 
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
-gem 'pg', '>= 0.18', '< 2.0'
+gem 'sqlite3'
 gem 'puma', '~> 4.1'
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_08_01_095312) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "employees", force: :cascade do |t|
     t.string "type"
     t.string "name"
